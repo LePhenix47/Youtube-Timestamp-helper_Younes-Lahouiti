8fe984473ec7223ae8bf99c042f044403d54fdb5
Younes Lahouiti <loller763@gmail.com>
2025-08-12 14:52:45 +0200

    Addition of styles for the CSS reset to include styles for range inputs with an overflowing thumb

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import "./sass/main.scss";
 
+function fixInputRangeBackground() {
+  const inputsWithThumbArray = document.querySelectorAll<HTMLInputElement>(
+    `input[type="range"][data-range-style="overflowing-thumb"]`
+  );
+
+  for (const input of inputsWithThumbArray) {
+    input.addEventListener("input", (e) => {
+      const input = e.currentTarget as HTMLInputElement;
+      const { min, max, valueAsNumber } = input;
+
+      const percentage: number = Math.round(
+        (valueAsNumber / Number(max)) * 100
+      );
+
+      const stringResult: string = `${percentage}%`;
+
+      input.style.setProperty("--_webkit-progression-width", stringResult);
+    });
+  }
+}
+
+fixInputRangeBackground();
+
 console.log("Hello world!");
