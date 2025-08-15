e9713555f2944c761eb585d0bc872b645a7f24cf
Younes Lahouiti <loller763@gmail.com>
2025-08-15 14:31:09 +0200

    Update on the main logic to use the abstraction

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import FileDropManager from "./classes/file-drop-manager.class";
 import Signal from "./classes/signal.class";
 import "./sass/main.scss";
 
@@ -16,86 +17,27 @@ const videoDropZoneInput = document.querySelector<HTMLInputElement>(
   "[data-element=upload-video-input]"
 );
 
-// ! WARNING: These events should NEVER trigger if a video was already uploaded (need to bear that in mind)
-// TODO: Create a class to abstract the dropzone stuff
-videoDropZoneInput.addEventListener("change", (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  if (!input?.files?.length) {
-    return;
-  }
+const fileDropManager = new FileDropManager(videoDropZone, videoDropZoneInput);
 
-  const file = input.files[0];
-  console.log(file);
-
+fileDropManager.setFileValidation((file) => {
   if (!file.type.startsWith("video/")) {
-    console.error("Invalid file type");
-    // TODO: Add signals that updates the UI
-    return;
+    return "Invalid file type. Please upload a video file.";
   }
+});
 
-  signal.emit("video:upload", {
-    file: file,
-    eventType: event.type,
+fileDropManager
+  .onFileUpload((file, eventType) => {
+    console.log("File uploaded:", file);
+    console.log("Event type:", eventType);
+  })
+  .onFileUploadError((file, errorMessage, eventType) => {
+    console.error("File upload error:", errorMessage);
   });
-});
 
-document.addEventListener("paste", (event: ClipboardEvent) => {
-  const { activeElement } = document;
-  const isFocusedOnADifferentInput =
-    activeElement !== videoDropZoneInput &&
-    activeElement.tagName.toLocaleLowerCase() !== "input";
-
-  if (isFocusedOnADifferentInput) {
-    return;
-  }
-
-  const items = event.clipboardData?.items;
-  if (!items) return;
-
-  const file = items[0].getAsFile();
-
-  signal.emit("video:upload", {
-    file: file,
-    eventType: event.type,
+fileDropManager
+  .onDragEnter(() => {
+    console.log("Drag enter");
+  })
+  .onDragLeave(() => {
+    console.log("Drag leave");
   });
-});
-
-videoDropZone.addEventListener("drop", (event: DragEvent) => {
-  event.preventDefault();
-
-  if (!event.dataTransfer?.files.length) {
-    return;
-  }
-
-  const file = event.dataTransfer.files[0];
-  console.log(file); // File object
-
-  signal.emit("video:upload", {
-    file: file,
-    eventType: event.type,
-  });
-});
-
-videoDropZone.addEventListener("dragover", (event: DragEvent) => {
-  event.preventDefault();
-});
-
-videoDropZone.addEventListener("dragleave", (event) => {
-  signal.emit("remove-dropzone-highlight");
-});
-
-videoDropZone.addEventListener("dragenter", (event) => {
-  event.preventDefault();
-
-  console.log(event.type);
-
-  signal.emit("add-dropzone-highlight");
-});
-
-videoDropZone.addEventListener("dragleave", (event: DragEvent) => {
-  event.preventDefault();
-
-  console.log(event.type);
-
-  signal.emit("remove-dropzone-highlight");
-});
