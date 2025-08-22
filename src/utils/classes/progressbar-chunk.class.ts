358c9bd4666d937c84a5bae75ff940e35793342a
Younes Lahouiti <loller763@gmail.com>
2025-08-22 19:40:47 +0200

    Implemented the progress chunkbar class

--- src/utils/classes/progressbar-chunk.class.ts
+++ src/utils/classes/progressbar-chunk.class.ts
@@ -1,18 +1,59 @@
 class ProgressBarChunk {
-  startTime: number;
-  endTime: number;
-  element: HTMLElement;
+  private startTime: number;
+  private endTime: number;
+  private element: HTMLLIElement;
+  private template: HTMLTemplateElement;
 
-  constructor(element: HTMLElement, startTime: number, endTime: number) {}
+  constructor(startTime: number, endTime: number) {
+    this.startTime = startTime;
+    this.endTime = endTime;
 
-  // Update visual progress for this chunk
-  updateProgress = (currentTime: number): void => {};
+    this.initializeTemplate();
+    this.element = this.chunkClone; // Assign the cloned element
+    this.updateChunkDOM(); // Safe to call now
+  }
 
-  // Update hover indicator
-  updateHover = (hoverTime: number): void => {};
+  private initializeTemplate = () => {
+    this.template = document.createElement("template");
 
-  // Update buffered progress
-  updateBuffered = (bufferEnd: number): void => {};
+    this.template.innerHTML = /*html*/ `
+      <li class="video__progress-bar-chunk" style="--_chunk-start-secs: 0; --_chunk-end-secs: 0;">
+        <button class="video__progress-drag-slide video__progress-drag-slide--start" disabled></button>
+        <button class="video__progress-drag-slide video__progress-drag-slide--end" disabled></button>
+        <div class="video__progress-chunk-progress"></div>
+        <div class="video__progress-chunk-progress video__progress-chunk-progress--hover-progress"></div>
+        <div class="video__progress-chunk-progress video__progress-chunk-progress--buffer-progress"></div>
+      </li>
+    `;
+  };
+
+  get chunkClone(): HTMLLIElement {
+    return this.template.content.firstElementChild!.cloneNode(
+      true
+    ) as HTMLLIElement;
+  }
+
+  private updateChunkDOM = () => {
+    this.element.style.setProperty("--_chunk-start-secs", `${this.startTime}`);
+    this.element.style.setProperty("--_chunk-end-secs", `${this.endTime}`);
+  };
+
+  public updateStartTime = (time: number): void => {
+    this.startTime = time;
+    this.updateChunkDOM();
+  };
+
+  public updateEndTime = (time: number): void => {
+    this.endTime = time;
+    this.updateChunkDOM();
+  };
+
+  public updateHover = (hoverTime: number): void => {
+    const hoverTimeIsWithinChunk: boolean =
+      hoverTime >= this.startTime && hoverTime <= this.endTime;
+
+    this.element.classList.toggle("hover-overlap", hoverTimeIsWithinChunk);
+  };
 }
 
 export default ProgressBarChunk;
